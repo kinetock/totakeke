ac0865ecc9fbe48be29ba03258c41bfb3f247351
Go Takagi <takagi@shimastripe.com>
2017-03-29 19:55:47 +0900

    Tweak

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
